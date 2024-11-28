d089f4ccfbdd8b1e4fd1095484dfbecff357d15a
mangjaki <cungcang996@gmail.com>
2024-11-28 09:48:43 +0700

    update

---
+++ resepmakanan/lib/data/makanan_data.dart
@@ -0,0 +1,32 @@
+import 'package:resepmakanan/models/makanan.dart';
+
+var makananList = [
+  Makanan(
+      nama: 'Nasi Goreng',
+      asal: 'Indonesia',
+      deskripsi: 'Nasi goreng mulai dikenal di Indonesia saat pedagang dari Tiongkok berkunjung ke Kerajaan Sriwijaya di abad ke-10. Sejak saat itu, nasi goreng dapat ditemukan di hampir seluruh pelosok Indonesia.',
+      built: 'Abad ke-10',
+      imageAsset: 'images/nasigoreng.jpg',
+      imageUrls: [
+        'https://cdn.rri.co.id/berita/Cirebon/o/1719201132146-ILUSTRASI_NASGOR/8w83zosucbx67ts.jpeg',
+        'https://i.ytimg.com/vi/HWYhoO8PCNQ/maxresdefault.jpg',
+        'https://cdn.idntimes.com/content-images/community/2022/07/fromandroid-4752b11d5e98386e9242ce8e2798dcd3_600x400.jpg',
+        'https://cdn.idntimes.com/content-images/community/2019/06/43913432-181263936115512-8488123280550676639-n-411471de1339ff50ac65be456ec74d37.jpg',
+      ],
+    isFavorite: false,
+  ),
+  Makanan(
+      nama: 'Rawon',
+      asal: 'Surabaya, Jawa Timur',
+      deskripsi: 'Rawon adalah sup daging sapi khas Indonesia yang berasal dari Jawa Timur. Sup ini memiliki ciri khas berupa kuah hitam pekat dan rasa asam gurih yang berasal dari bumbu kacang keluak.',
+      built: '901 M',
+      imageAsset: 'images/rawon.jpg',
+      imageUrls: [
+        'https://cdn.idntimes.com/content-images/community/2022/05/fromandroid-9e9e92b57ee8fce6444c5c6e0af25e19.jpg',
+        'https://cdn.idntimes.com/content-images/community/2022/05/fromandroid-ae279a0d80b93b094e720d18409da05a_600x400.jpg',
+        'https://assets-a1.kompasiana.com/items/album/2021/06/28/rawon-kompascom-60d9194b06310e417843cb92.jpg',
+        'https://doyanayam.com/wp-content/uploads/2024/01/Screenshot_4.png',
+      ],
+    isFavorite: false,
+  ),
+];
